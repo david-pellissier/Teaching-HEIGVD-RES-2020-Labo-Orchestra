476821ac9f7598008c0d9d10b4e1f835701ce150
quillasp <saralfddin93@yahoo.fr>
2021-06-19 14:46:07 +0200

    Implementation of an Auditor

---
+++ docker/image-auditor/src/protocol.js
@@ -0,0 +1,5 @@
+// Ptite définition de ce qu'on utilisera pour la communination
+exports.UDP_PORT = "10001"; //une idée de SLO, because wtfn ?
+exports.TCP_PORT = "2205"
+exports.MULTICAST_ADDRESS = "239.255.10.10";
+exports.TCP_ADDRESS = "127.0.0.1";
